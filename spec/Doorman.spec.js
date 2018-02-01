8cee2041d823c2a56712178975c5a233b19eb461
Sascha <sascha@gurkware.de>
2018-02-01 16:39:42 +0100

    TEST: rightsMap test

--- spec/Doorman.spec.js
+++ spec/Doorman.spec.js
@@ -7,5 +7,17 @@ describe('Doorman', () => {
       let door = new Doorman([])
       expect(door.rightsMap).toEqual({})
     })
+    it('should throw error on cyclic permissions', () => {
+      expect(() => {new Doorman([['1', '2'], ['2', '1']])})
+        .toThrow(new Error('Cyclic permissions, are not allowed'))
+    })
+    it('should parse nesting', () => {
+      let door = new Doorman([['1', '2'], ['2', '3']])
+      expect(door.rightsMap).toEqual({
+        '1': ['2', '3'],
+        '2': ['3'],
+        '3': []
+      })
+    })
   })
 })
